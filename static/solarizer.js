59d2cdaa55906c61923a7fa68ee405490d1262da
K Bertrand <leopeny.k@gmail.com>
2016-02-11 14:08:32 -0600

    add starter js and css files

---
+++ static/solarizer.js
@@ -0,0 +1,7 @@
+$(function(){
+  //sett his hup eventually to just attach the colorpicker to any child element of a parent container
+  //no way I'm manually adding all of them
+  $('#base3').colorpicker();
+  $('#base2').colorpicker();
+  $('#base1').colorpicker();
+});
