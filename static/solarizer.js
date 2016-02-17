71a33eb14c62ef362a96152102b029b0f0a0e3fd
K Bertrand <leopeny.k@gmail.com>
2016-02-16 23:33:45 -0600

    colorpicker change to spectrum and basic styling

--- static/solarizer.js
+++ static/solarizer.js
@@ -1,7 +1,22 @@
 $(function(){
-  //sett his hup eventually to just attach the colorpicker to any child element of a parent container
-  //no way I'm manually adding all of them
-  $('#base3').colorpicker();
-  $('#base2').colorpicker();
-  $('#base1').colorpicker();
+/*  var solarized_defaults = {
+    base03: "002b36",
+    base02: "#073642",
+    base01: "#586e75",
+    base00: "#657b83",
+    base0: "#839496",
+    base1: "#93a1a1",
+    base2: "#eee8d5",
+    base3: "#fdf6e3",
+    yellow: "#b58900",
+    orange: "#cb4b16",
+    red: "#dc322f",
+    magenta: "#d33682",
+    violet: "#6c71c4",
+    blue: "#268bd2",
+    cyan: "#2aa198",
+    green: "#859900"
+  };
+*/
+$('.color').spectrum();
 });
